0ad039cc08cf28fe1861d5591dcb448b5c37f64f
Bryan McNamara <bryan@bkm-tech.com>
2023-04-22 16:41:48 -0700

    auth middleware

--- birdhouse-api/routes/login.js
+++ birdhouse-api/routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/loginController');
-const { authenticate } = require('../middleware/auth');
+const auth = require('../middleware/auth');
 const User = require('../models/User');
 
 router.get('/', loginController.getLogin);
