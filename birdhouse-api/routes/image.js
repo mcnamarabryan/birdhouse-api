e94db78eb76ed976209f1d4d170b0c0ce1e9bfb0
Bryan McNamara <bryan@bkm-tech.com>
2023-04-22 16:22:37 -0700

    remove errant char

--- birdhouse-api/routes/image.js
+++ birdhouse-api/routes/image.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const imageController = require('../controllers/imageController');
-const auth = require('../middlewares/auth');
+const auth = require('../middleware/auth');
 
 router.get('/get', auth, imageController.getImage);
 
