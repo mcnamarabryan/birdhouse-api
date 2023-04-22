bb5499ad07dfe7a4d2fb27ae189c1bf215fc649c
Bryan McNamara <bryan@bkm-tech.com>
2023-04-22 16:38:34 -0700

    fix another middelwares

--- birdhouse-api/routes/message.js
+++ birdhouse-api/routes/message.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messageController');
-const auth = require('../middlewares/auth');
+const auth = require('../middleware/auth');
 
 router.get('/get', auth, messageController.getMessage);
 
