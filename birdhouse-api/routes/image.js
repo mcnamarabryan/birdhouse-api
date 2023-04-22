9a85215e4d9d65ee7338bb275aa54f0d62071862
Bryan McNamara <bryan@bkm-tech.com>
2023-04-22 14:58:57 -0700

    Add auth middleware

--- birdhouse-api/routes/image.js
+++ birdhouse-api/routes/image.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const imageController = require('../controllers/imageController');
+const auth = require('../middlewares/auth');
 
-router.get('/get', imageController.getImage);
+router.get('/get', auth, imageController.getImage);
 
-router.post('/post', imageController.multerUpload, imageController.postImage);
+router.post('/post', auth, imageController.multerUpload, imageController.postImage);
 
 module.exports = router;
--- birdhouse-api/routes/message.js
+++ birdhouse-api/routes/message.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messageController');
+const auth = require('../middlewares/auth');
 
-router.get('/get', messageController.getMessage);
+router.get('/get', auth, messageController.getMessage);
 
-router.post('/post', messageController.postMessage);
+router.post('/post', auth, messageController.postMessage);
 
 module.exports = router;
