4dac2023fb52691f9b05ccd46d111fc7f6c8de4d
Bryan McNamara <bryan@bkm-tech.com>
2023-04-22 17:35:43 -0700

    chatgpt gixing user model

--- birdhouse-api/models/User.js
+++ birdhouse-api/models/User.js
@@ -1,26 +1,13 @@
 const connection = require('../config/database');
 
 class User {
-  static async findOne(condition) {
-    return new Promise((resolve, reject) => {
-      const where = Object.entries(condition)
-        .map(([key, value]) => `${key} = ${connection.escape(value)}`)
-        .join(' AND ');
-
-      const query = `SELECT * FROM users WHERE ${where} LIMIT 1`;
-
-      connection.query(query, (error, results) => {
-        if (error) {
-          return reject(error);
-        }
-
-        if (results.length > 0) {
-          resolve(results[0]);
-        } else {
-          resolve(null);
-        }
-      });
-    });
+  static async findOne(query) {
+    try {
+      const [rows] = await pool.query('SELECT * FROM users WHERE ?', [query]);
+      return rows[0] ? new User(rows[0]) : null;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
