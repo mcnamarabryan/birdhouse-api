466ba2ca7162fc6400667bd2404bbd358fadfcac
Bryan McNamara <bryan@bkm-tech.com>
2023-04-22 17:54:12 -0700

    user model constructor

--- birdhouse-api/models/User.js
+++ birdhouse-api/models/User.js
@@ -1,6 +1,13 @@
 const connection = require('../config/database');
 
 class User {
+  
+  constructor(data) {
+    this.id = data.id;
+    this.username = data.username;
+    this.password = data.password;
+  }
+
   static async findOne(query) {
     try {
       const [rows] = await connection.query('SELECT * FROM users WHERE ?', [query]);
