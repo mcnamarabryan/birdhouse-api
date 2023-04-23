3ca2472d71000eacb5f1f218a64ba94bc42da891
Bryan McNamara <bryan@bkm-tech.com>
2023-04-22 17:41:37 -0700

    change var name

--- birdhouse-api/models/User.js
+++ birdhouse-api/models/User.js
@@ -3,7 +3,7 @@ const connection = require('../config/database');
 class User {
   static async findOne(query) {
     try {
-      const [rows] = await pool.query('SELECT * FROM users WHERE ?', [query]);
+      const [rows] = await connection.query('SELECT * FROM users WHERE ?', [query]);
       return rows[0] ? new User(rows[0]) : null;
     } catch (error) {
       throw error;
